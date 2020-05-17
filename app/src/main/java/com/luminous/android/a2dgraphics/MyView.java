70b424564333f2872e263681ebf5fd9ce59e1dec
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-17 16:30:37 +0600

    init

---
+++ app/src/main/java/com/luminous/android/a2dgraphics/MyView.java
@@ -0,0 +1,11 @@
+package com.luminous.android.a2dgraphics;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+}
