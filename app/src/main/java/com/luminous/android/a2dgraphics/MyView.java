0b6b7b78eda87dc6f6e001805b455dd961e54ca7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-17 16:31:55 +0600

    MyView

--- app/src/main/java/com/luminous/android/a2dgraphics/MyView.java
+++ app/src/main/java/com/luminous/android/a2dgraphics/MyView.java
@@ -1,9 +1,12 @@
 package com.luminous.android.a2dgraphics;
 
 import android.content.Context;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
+
+    private Paint redPaint;
 
     public MyView(Context context) {
         super(context);
