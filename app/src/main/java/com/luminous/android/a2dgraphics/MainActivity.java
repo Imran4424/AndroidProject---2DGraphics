90869ce8126dfc62a290058f4c0377455afb37bb
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-17 18:10:40 +0600

    MyView

--- app/src/main/java/com/luminous/android/a2dgraphics/MainActivity.java
+++ app/src/main/java/com/luminous/android/a2dgraphics/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyView myView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        // replace the view with custom design view
     }
 }
