08d69f203b172f2b52150931341d6efe27beffef
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-19 13:11:04 +0600

    transformation view

--- app/src/main/java/com/luminous/android/a2dgraphics/TransformationView.java
+++ app/src/main/java/com/luminous/android/a2dgraphics/TransformationView.java
@@ -10,5 +10,13 @@ public class TransformationView extends View {
 
     public TransformationView(Context context) {
         super(context);
+
+        points = new Point[5];
+        points[0] = new Point(50, 300);
+        points[0] = new Point(150, 400);
+        points[0] = new Point(180, 340);
+        points[0] = new Point(240, 420);
+        points[0] = new Point(300, 200);
+
     }
 }
