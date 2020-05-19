7cc03ce651805a5d0687a864d6199eafd3007180
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-19 13:08:02 +0600

    transformation view

--- app/src/main/java/com/luminous/android/a2dgraphics/TransformationView.java
+++ app/src/main/java/com/luminous/android/a2dgraphics/TransformationView.java
@@ -1,9 +1,12 @@
 package com.luminous.android.a2dgraphics;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.view.View;
 
 public class TransformationView extends View {
+
+    Point[] points;
 
     public TransformationView(Context context) {
         super(context);
