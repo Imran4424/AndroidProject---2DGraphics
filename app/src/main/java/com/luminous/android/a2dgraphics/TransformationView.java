9c9b173b3e0220fb35cac7cd4037c9d55254c5d5
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-05-19 13:52:06 +0600

    transformation view

--- app/src/main/java/com/luminous/android/a2dgraphics/TransformationView.java
+++ app/src/main/java/com/luminous/android/a2dgraphics/TransformationView.java
@@ -87,5 +87,8 @@ public class TransformationView extends View {
         super.onDraw(canvas);
 
         canvas.drawPath(myLines, gradientPaint);
+        Point[] newPoints = translate(points, 20, 40);
+        updatePath(newPoints);
+        canvas.drawPath(myLines, blackBorderPaint);
     }
 }
